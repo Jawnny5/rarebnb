f1e7386f8e94f16e7ac148c623ff185188ced283
“Jawnny5” <william.neal83@gmail.com>
2021-11-14 09:54:22 -0700

    begin conversion of static html files to go templates

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,11 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/contact", handlers.Repo.Contact)
+	mux.Get("/hurtyurt", handlers.Repo.HurtYurt)
+	mux.Get("/timothys", handlers.Repo.Timothys)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	//fileServer creates a means of serving static files to application
 	fileServer := http.FileServer(http.Dir("./static/"))
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -37,3 +37,28 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{StringMap: stringMap})
 }
+
+//Reservation renders the make a reservation page and displays form
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+//Reservation renders the make a reservation page and displays form
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
+}
+
+//Availability renders the search availability room
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
+}
+
+//Timothys renders the page for the "Timothy's Chalet"
+func (m *Repository) Timothys(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "timothys.page.tmpl", &models.TemplateData{})
+}
+
+//HurtYurt renders the page for "The Hurt Yurt"
+func (m *Repository) HurtYurt(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "hurtyurt.page.tmpl", &models.TemplateData{})
+}
